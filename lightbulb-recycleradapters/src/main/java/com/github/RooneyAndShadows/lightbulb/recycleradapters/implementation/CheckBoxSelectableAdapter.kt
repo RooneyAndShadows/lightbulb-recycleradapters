6d28ec48ae29a1bb72297e8323bf23b6b99740f3
rooney123777 <rooney123777@gmail.com>
2023-01-20 20:22:35 +0200

    Predefined adaprters fixes.

--- lightbulb-recycleradapters/src/main/java/com/github/RooneyAndShadows/lightbulb/recycleradapters/implementation/CheckBoxSelectableAdapter.kt
+++ lightbulb-recycleradapters/src/main/java/com/github/RooneyAndShadows/lightbulb/recycleradapters/implementation/CheckBoxSelectableAdapter.kt
@@ -1,9 +1,10 @@
 package com.github.rooneyandshadows.lightbulb.recycleradapters.implementation
 
+import android.content.Context
 import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
 import com.github.rooneyandshadows.lightbulb.recycleradapters.R
 import com.github.rooneyandshadows.lightbulb.recycleradapters.abstraction.EasyAdapterDataModel
 import com.github.rooneyandshadows.lightbulb.recycleradapters.abstraction.EasyAdapterSelectableModes.*
@@ -12,67 +13,72 @@ import com.github.rooneyandshadows.lightbulb.selectableview.CheckBoxView
 
 @Suppress("UNUSED_PARAMETER", "unused", "MemberVisibilityCanBePrivate")
 open class CheckBoxSelectableAdapter<ItemType : EasyAdapterDataModel> : EasyRecyclerAdapter<ItemType>(SELECT_MULTIPLE) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val v = LayoutInflater.from(parent.context)
-            .inflate(R.layout.layout_checkbox_button, parent, false) as CheckBoxView
-        val padding = getItemPadding()
-        if (padding != null && padding.size == 4) v.setPadding(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val checkBoxView = LayoutInflater.from(parent.context).inflate(
+            R.layout.layout_checkbox_button,
+            parent, false
+        ) as CheckBoxView
+        val padding = getItemPadding(context)
+        if (padding != null && padding.size == 4) checkBoxView.setPadding(
             padding[0],
             padding[1],
             padding[2],
             padding[3]
         )
-        return CheckBoxViewHolder(v, this)
+        return CheckBoxViewHolder(checkBoxView)
     }
 
     @Suppress("UNCHECKED_CAST")
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    @Override
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val vHolder: CheckBoxViewHolder = holder as CheckBoxSelectableAdapter<ItemType>.CheckBoxViewHolder
         vHolder.bindItem()
     }
 
     @Suppress("UNCHECKED_CAST")
-    override fun onViewRecycled(holder: RecyclerView.ViewHolder) {
+    @Override
+    override fun onViewRecycled(holder: ViewHolder) {
         val vh: CheckBoxViewHolder = holder as CheckBoxSelectableAdapter<ItemType>.CheckBoxViewHolder
         vh.recycle()
     }
 
+    @Override
     override fun getItemCount(): Int {
         return getItems().size
     }
 
+    @Override
     override fun getItemName(item: ItemType): String? {
         return item.itemName
     }
 
-    protected open fun getItemIcon(item: ItemType): Drawable? {
+    protected open fun getItemIcon(context: Context, item: ItemType): Drawable? {
         return null
     }
 
-    protected open fun getItemIconBackground(item: ItemType): Drawable? {
+    protected open fun getItemIconBackground(context: Context, item: ItemType): Drawable? {
         return null
     }
 
-    protected open fun getItemPadding(): IntArray? {
+    protected open fun getItemPadding(context: Context): IntArray? {
         return null
     }
 
-    inner class CheckBoxViewHolder internal constructor(
-        categoryItemBinding: CheckBoxView?,
-        adapter: EasyRecyclerAdapter<ItemType>,
-    ) : RecyclerView.ViewHolder(
-        categoryItemBinding!!
-    ) {
+    inner class CheckBoxViewHolder internal constructor(checkBoxView: CheckBoxView) : ViewHolder(checkBoxView) {
         private var selectableView: CheckBoxView = itemView as CheckBoxView
-        private var item: ItemType? = null
 
         fun bindItem() {
-            item = getItem(bindingAdapterPosition) ?: return
-            val isSelectedInAdapter = isItemSelected(item!!)
-            if (selectableView.isChecked != isSelectedInAdapter) selectableView.isChecked =
-                isSelectedInAdapter
-            selectableView.text = getItemName(item!!)
-            selectableView.setIcon(getItemIcon(item!!), getItemIconBackground(item!!))
+            selectableView.apply {
+                val item = getItem(bindingAdapterPosition) ?: return
+                val isSelectedInAdapter = isItemSelected(item)
+                val itemText = getItemName(item)
+                val itemIcon = getItemIcon(context, item)
+                val itemIconBackground = getItemIconBackground(context, item)
+                if (isChecked != isSelectedInAdapter) isChecked = isSelectedInAdapter
+                text = itemText
+                setIcon(itemIcon, itemIconBackground)
+            }
         }
 
         fun recycle() {
@@ -82,7 +88,7 @@ open class CheckBoxSelectableAdapter<ItemType : EasyAdapterDataModel> : EasyRecy
         init {
             selectableView.setOnCheckedListener { _: CheckBoxView?, isChecked: Boolean ->
                 selectableView.post {
-                    adapter.selectItemAt(bindingAdapterPosition, isChecked)
+                    selectItemAt(bindingAdapterPosition, isChecked)
                 }
             }
         }
--- lightbulb-recycleradapters/src/main/java/com/github/RooneyAndShadows/lightbulb/recycleradapters/implementation/RadioButtonSelectableAdapter.kt
+++ lightbulb-recycleradapters/src/main/java/com/github/RooneyAndShadows/lightbulb/recycleradapters/implementation/RadioButtonSelectableAdapter.kt
@@ -1,5 +1,6 @@
 package com.github.rooneyandshadows.lightbulb.recycleradapters.implementation
 
+import android.content.Context
 import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -13,64 +14,74 @@ import com.github.rooneyandshadows.lightbulb.selectableview.RadioButtonView
 @Suppress("UNUSED_PARAMETER", "unused", "MemberVisibilityCanBePrivate")
 open class RadioButtonSelectableAdapter<ItemType : EasyAdapterDataModel> :
     EasyRecyclerAdapter<ItemType>(EasyAdapterSelectableModes.SELECT_SINGLE) {
+    @Override
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val v = LayoutInflater.from(parent.context)
-            .inflate(R.layout.layout_radio_button, parent, false) as RadioButtonView
-        val padding = getItemPadding()
-        if (padding != null && padding.size == 4) v.setPadding(
+        val context = parent.context
+        val radioButtonView = LayoutInflater.from(parent.context).inflate(
+            R.layout.layout_radio_button,
+            parent,
+            false
+        ) as RadioButtonView
+        val padding = getItemPadding(context)
+        if (padding != null && padding.size == 4) radioButtonView.setPadding(
             padding[0],
             padding[1],
             padding[2],
             padding[3]
         )
-        return RadioButtonViewHolder(v)
+        return RadioButtonViewHolder(radioButtonView)
     }
 
     @Suppress("UNCHECKED_CAST")
+    @Override
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val vHolder: RadioButtonViewHolder = holder as RadioButtonSelectableAdapter<ItemType>.RadioButtonViewHolder
         vHolder.bindItem()
     }
 
     @Suppress("UNCHECKED_CAST")
+    @Override
     override fun onViewRecycled(holder: RecyclerView.ViewHolder) {
         val vh: RadioButtonViewHolder = holder as RadioButtonSelectableAdapter<ItemType>.RadioButtonViewHolder
         vh.recycle()
     }
 
+    @Override
     override fun getItemCount(): Int {
         return getItems().size
     }
 
+    @Override
     override fun getItemName(item: ItemType): String {
         return item.itemName
     }
 
-    protected open fun getItemIcon(item: ItemType): Drawable? {
+    protected open fun getItemIcon(context: Context, item: ItemType): Drawable? {
         return null
     }
 
-    protected open fun getItemIconBackground(item: ItemType): Drawable? {
+    protected open fun getItemIconBackground(context: Context, item: ItemType): Drawable? {
         return null
     }
 
-    protected open fun getItemPadding(): IntArray? {
+    protected open fun getItemPadding(context: Context): IntArray? {
         return null
     }
 
-    inner class RadioButtonViewHolder(categoryItemBinding: RadioButtonView?) :
-        RecyclerView.ViewHolder(
-            categoryItemBinding!!
-        ) {
+    inner class RadioButtonViewHolder(radioButtonView: RadioButtonView) : RecyclerView.ViewHolder(radioButtonView) {
         private var selectableView: RadioButtonView = itemView as RadioButtonView
-        private var item: ItemType? = null
+
         fun bindItem() {
-            item = getItem(bindingAdapterPosition) ?: return
-            val isSelectedInAdapter = isItemSelected(item!!)
-            if (selectableView.isChecked != isSelectedInAdapter) selectableView.isChecked =
-                isSelectedInAdapter
-            selectableView.text = getItemName(item!!)
-            selectableView.setIcon(getItemIcon(item!!), getItemIconBackground(item!!))
+            selectableView.apply {
+                val item = getItem(bindingAdapterPosition) ?: return
+                val isSelectedInAdapter = isItemSelected(item)
+                val itemText = getItemName(item)
+                val itemIcon = getItemIcon(context, item)
+                val itemIconBackground = getItemIconBackground(context, item)
+                if (isChecked != isSelectedInAdapter) isChecked = isSelectedInAdapter
+                text = itemText
+                setIcon(itemIcon, itemIconBackground)
+            }
         }
 
         fun recycle() {
@@ -80,10 +91,7 @@ open class RadioButtonSelectableAdapter<ItemType : EasyAdapterDataModel> :
         init {
             selectableView.setOnCheckedListener { _: RadioButtonView?, isChecked: Boolean ->
                 selectableView.post {
-                    selectItem(
-                        item!!,
-                        isChecked
-                    )
+                    selectItemAt(bindingAdapterPosition, isChecked)
                 }
             }
         }
