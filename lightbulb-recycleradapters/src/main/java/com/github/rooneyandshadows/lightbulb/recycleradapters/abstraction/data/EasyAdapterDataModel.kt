f33513a0455756cc4b55241a5c91935ecfe1bf29
rooney123777 <rooney123777@gmail.com>
2024-01-17 20:01:44 +0200

    version 2.3.0 release. - EasyAdapterDataModel empty implementation added.

--- lightbulb-recycleradapters/src/main/java/com/github/rooneyandshadows/lightbulb/recycleradapters/abstraction/data/EasyAdapterDataModel.kt
+++ lightbulb-recycleradapters/src/main/java/com/github/rooneyandshadows/lightbulb/recycleradapters/abstraction/data/EasyAdapterDataModel.kt
@@ -1,7 +1,15 @@
 package com.github.rooneyandshadows.lightbulb.recycleradapters.abstraction.data
 
+import android.os.Parcel
 import android.os.Parcelable
 
 abstract class EasyAdapterDataModel : Parcelable {
     abstract val itemName: String
+
+    override fun describeContents(): Int {
+        return 0;
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+    }
 }
